b29c08b853a24cb47ca365057798730c119ecfa4
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2024-03-26 20:45:18 +0800

    Make improvements

---
+++ crypt.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell -executionpolicy bypass -file "%~dp0crypt.ps1" %*
--- crypt.ps1
+++ crypt.ps1
@@ -1,12 +1,19 @@
-param([string]$path)
+param (
+    [string]$path,
+    [string]$output
+)
 
 if (-not $path) {
     Write-Host "[-] Error: Please provide the Python file path using the -path parameter." -ForegroundColor Red
-    Start-Sleep 10
+    exit
 }
 
 $pyscr = Get-Content -Path $path -Raw
 $encscr = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($pyscr))
 $randchr = [char](Get-Random -Minimum 65 -Maximum 91)
 $impr = "import base64;${randchr}='${encscr}';${randchr}=${randchr}.replace('*','${replace}');exec(base64.b64decode(${randchr}))"
-Write-Output $impr
+if (-not $output) {
+    Write-Output $impr
+} else {
+    Write-Output $impr | Out-File -FilePath $output -Encoding utf8
+}
